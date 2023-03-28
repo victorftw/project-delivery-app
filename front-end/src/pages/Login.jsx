0ecaeddf9cb1e158c56df4466c007b19e6f6a647
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2023-03-28 17:52:07 -0300

    feat: colocando elementos do login (req 2)

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React from "react";
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>
+      <img src="" alt="" />
+      <h1>nome app</h1>
+      <form>
+        <label htmlFor="">Login</label>
+        <input type="email" data-testid="common_login__input-email" />
+
+        <label htmlFor="">Senha</label>
+        <input type="password" data-testid="common_login__input-password" />
+
+        <button type="submit" data-testid="common_login__button-login">LOGIN</button>
+        <button data-testid="common_login__button-register">Ainda não tenho conta</button>
+      </form>
+      <p data-testid="common_login__element-invalid-email">Mensagem de erro</p>
+    </div>
   );
 }
 
