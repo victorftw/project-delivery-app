0bec0953ce2c5ecca122e8c59726f83fc6034873
Gustavo-Aquino-1 <guhaquino10@gmail.com>
2023-03-28 18:10:05 -0300

    feat: requisito 3

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 function Login() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isBtnDisabled, setIsBtnDisabled] = useState(true);
+
+  useEffect(() => {
+    const regex = /\S+[@]\w+[.]\w+/gi;
+    if(regex.test(email) && password.length >= 6) setIsBtnDisabled(false)
+    else setIsBtnDisabled(true);
+  }, [email, password])
+
   return (
     <div>
       <img src="" alt="" />
       <h1>nome app</h1>
       <form>
         <label htmlFor="">Login</label>
-        <input type="email" data-testid="common_login__input-email" />
+        <input 
+          type="email"
+          data-testid="common_login__input-email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
 
         <label htmlFor="">Senha</label>
-        <input type="password" data-testid="common_login__input-password" />
+        <input
+          type="password"
+          data-testid="common_login__input-password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
 
-        <button type="submit" data-testid="common_login__button-login">LOGIN</button>
+        <button disabled={isBtnDisabled} type="submit" data-testid="common_login__button-login">LOGIN</button>
         <button data-testid="common_login__button-register">Ainda não tenho conta</button>
       </form>
       <p data-testid="common_login__element-invalid-email">Mensagem de erro</p>
