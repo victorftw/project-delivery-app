ebaff6f38506e860726d7ad0b07bbcfcb5891d20
Victor Hugo <victor.hiraiftw@outlook.com>
2023-03-29 15:41:48 -0300

    feat: rota de cadastro req 6

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Login from './pages/Login';
 import CustomerProducts from './pages/CustomerProducts';
+import Register from './pages/Register';
 
 function App() {
   return (
     <Switch>
       <Route exact path="/customer/products" component={ CustomerProducts } />
       <Route exact path="/login" component={ Login } />
+      <Route exact path="/register" component={ Register } />
       <Redirect exact from="/" to="/login" />
     </Switch>
   );
