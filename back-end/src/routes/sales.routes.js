28146bbe0b4b5bdf61fd45b4bb601fcc1f449e7c
Victor Hugo <victor.hiraiftw@outlook.com>
2023-03-31 16:30:55 -0300

    terminando mesclagem

--- back-end/src/routes/sales.routes.js
+++ back-end/src/routes/sales.routes.js
@@ -1,9 +1,11 @@
 const { Router } = require('express');
 const control = require('../controllers/sale.controller');
 
+const verify = require('../middlewares/jwt.verify.token');
+
 const salesRoutes = Router();
 
-salesRoutes.post('/sale', control.create);
-salesRoutes.get('/sale/:id', control.getSales);
+salesRoutes.post('/sale', verify, control.create);
+salesRoutes.get('/sale/:id', verify, control.getSales);
 
 module.exports = salesRoutes;
