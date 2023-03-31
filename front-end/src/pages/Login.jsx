c8c1b1cc51e96d74ab013b0aa101c4e350d691ce
Victor Hugo <victor.hiraiftw@outlook.com>
2023-03-31 19:54:48 -0300

    fix: bug do req 13 resolvido

--- front-end/src/pages/Login.jsx
+++ front-end/src/pages/Login.jsx
@@ -1,23 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom';
-import useLocalStorage from '../hooks/useLocalStorage';
+// import useLocalStorage from '../hooks/useLocalStorage';
 
 function Login() {
-  const { state: user } = useLocalStorage('user', {});
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [isBtnDisabled, setIsBtnDisabled] = useState(true);
   const [isUserNotFound, setIsUserNotFound] = useState(false);
   const { push } = useHistory();
   useEffect(() => {
-    if (user.email) {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user?.email) {
       push('/customer/products');
     }
     const seis = 6;
     const regex = /\S+[@]\w+[.]\w+/gi;
     if (regex.test(email) && password.length >= seis) setIsBtnDisabled(false);
     else setIsBtnDisabled(true);
-  }, [email, password, user.email, push]);
+  }, [email, password, push]);
   const handleClick = async () => {
     const response = await fetch('http://localhost:3001/login', {
       method: 'POST',
