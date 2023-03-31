7d1c428a9a28ce70703bce6939c430360c9a2b8a
Lucas Gomes Filgueiras Tavares <lucasgft200303@gmail.com>
2023-03-31 18:51:55 -0300

    feat: rota e pagina customer-orders-details

--- back-end/src/routes/sale.routes.js
+++ back-end/src/routes/sale.routes.js
@@ -1,10 +1,12 @@
 const { Router } = require('express');
-const control = require('../controllers/sale.controller');
+const { create, getById, updateStatusSales } = require('../controllers/sale.controller');
 
 const verify = require('../middlewares/jwt.verify.token');
 
 const salesRoutes = Router();
 
-salesRoutes.post('/sale', verify, control.create);
+salesRoutes.get('/customer/orders/:idVenda', getById);
+salesRoutes.post('/sale', verify, create);
+salesRoutes.patch('/customer/orders/:idVenda', updateStatusSales);
 
 module.exports = salesRoutes;
