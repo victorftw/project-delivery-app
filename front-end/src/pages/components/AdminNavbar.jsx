9d0d320d0ead10e2cf1fba91892e64ff81d157ad
Victor Hugo <victor.hiraiftw@outlook.com>
2023-05-29 11:50:22 -0300

    style: css admin navbar

--- front-end/src/pages/components/AdminNavbar.jsx
+++ front-end/src/pages/components/AdminNavbar.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
 import useLocalStorage from '../../hooks/useLocalStorage';
+import '../../css/AdminNavbar.css';
 
 export default function AdminNavbar() {
   const { state: user } = useLocalStorage('user', {});
   const history = useHistory();
   return (
-    <header>
-      <div>
+    <header className="container-nav">
+      <div className="nav-admin">
         <button
+          className="buttonAdminNavbar select"
           data-testid="customer_products__element-navbar-link-orders"
           type="button"
           onClick={ () => history.push('/admin/manage') }
@@ -16,8 +18,8 @@ export default function AdminNavbar() {
           Gerenciar Usuários
         </button>
       </div>
-
-      <div>
+      <div style={ { backgroundColor: '#036b52', width: '100%' } } />
+      <div className="nav-user">
         <div data-testid="customer_products__element-navbar-user-full-name">
           {user.name}
         </div>
